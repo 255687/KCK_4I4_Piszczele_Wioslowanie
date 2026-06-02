698a5f1162ab2e67ce81d04368a129fac1e14651
Kacper <255596@edu.p.lodz.pl>
2026-06-02 22:06:50 +0200

    Aktualizacja tworzenia bazy danych do potrzeby nowego tworzenia i dzielenia treningow

--- database.py
+++ database.py
@@ -9,27 +9,43 @@ class Database:
     def init_db(self):
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS statystyki (
+        c.execute('''CREATE TABLE IF NOT EXISTS treningi (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        data TEXT,
+                        data TEXT
+                    )''')
+        c.execute('''CREATE TABLE IF NOT EXISTS serie (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        trening_id INTEGER,
+                        numer_serii INTEGER,
                         poprawne INTEGER,
-                        bledne INTEGER
+                        bledne INTEGER,
+                        FOREIGN KEY(trening_id) REFERENCES treningi(id)
                     )''')
         conn.commit()
         conn.close()
 
-    def zapisz_trening(self, poprawne, bledne):
+    def nowy_trening(self):
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
-        c.execute("INSERT INTO statystyki (data, poprawne, bledne) VALUES (?, ?, ?)",
-                  (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), poprawne, bledne))
+        data_teraz = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        c.execute("INSERT INTO treningi (data) VALUES (?)", (data_teraz,))
+        trening_id = c.lastrowid
+        conn.commit()
+        conn.close()
+        return trening_id
+
+    def zapisz_serie(self, trening_id, numer_serii, poprawne, bledne):
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+        c.execute("INSERT INTO serie (trening_id, numer_serii, poprawne, bledne) VALUES (?, ?, ?, ?)",
+                  (trening_id, numer_serii, poprawne, bledne))
         conn.commit()
         conn.close()
 
-    def pobierz_statystyki(self, limit=5):
+    def pobierz_serie_treningu(self, trening_id):
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
-        c.execute("SELECT data, poprawne, bledne FROM statystyki ORDER BY id DESC LIMIT ?", (limit,))
+        c.execute("SELECT numer_serii, poprawne, bledne FROM serie WHERE trening_id = ? ORDER BY numer_serii ASC", (trening_id,))
         dane = c.fetchall()
         conn.close()
         return dane
